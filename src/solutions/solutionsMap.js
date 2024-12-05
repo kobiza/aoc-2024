ea514224fc37bf607dc03bb74aa3475a88a82ffd
kobiza <kobiz@wix.com>
2024-12-05 09:25:55 +0200

    day 05 new

--- src/solutions/solutionsMap.js
+++ src/solutions/solutionsMap.js
@@ -1,9 +1,11 @@
 import day02 from './day02/index'
 import day03 from './day03/index'
 import day04 from './day04/index'
+import day05 from './day05/index'
 
 export default {
 	day02,
 	day03,
 	day04,
+	day05,
 }
