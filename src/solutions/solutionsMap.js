6a7273dc997f807ece7c6e0e5764227a50d265f4
kobiza <kobiz@wix.com>
2024-12-07 13:53:03 +0200

    day 07 add

--- src/solutions/solutionsMap.js
+++ src/solutions/solutionsMap.js
@@ -3,6 +3,7 @@ import day03 from './day03/index'
 import day04 from './day04/index'
 import day05 from './day05/index'
 import day06 from './day06/index'
+import day07 from './day07/index'
 
 export default {
 	day02,
@@ -10,4 +11,5 @@ export default {
 	day04,
 	day05,
 	day06,
+	day07,
 }
