35111a387c0ab0473c93ce0e666ef01b4ba53207
kobiza <kobiz@wix.com>
2024-12-09 09:03:22 +0200

    day 09 solve part 1

--- src/solutions/day09/example.js
+++ src/solutions/day09/example.js
@@ -1,3 +1,3 @@
-const example = ``
+const example = `2333133121414131402`
 
 export default example
--- src/solutions/day09/index.js
+++ src/solutions/day09/index.js
@@ -4,10 +4,63 @@ import example from './example'
 import {part1Tests, part2Tests} from './testsData'
 import testRunner from "../../utils/testRunner";
 
-const inputParser = (input) => input.split('\n').filter(v => v)
+const inputParser = (input) => input.split('')
 
 const part1 = (input) => {
-    return _.identity(input)
+    const getP1 = (arr) => {
+        const result = []
+
+        let isFile = true
+        let filesCounter = -1
+        for (let i = 0; i < arr.length; i++) {
+            const repeat = parseInt(arr[i])
+
+            if (isFile) {
+                filesCounter++
+            }
+            const cToAdd = isFile ? `${filesCounter}` : '.'
+            for (let j = 0; j < repeat; j++) {
+                result.push(cToAdd)
+            }
+            isFile = !isFile
+        }
+
+        return result
+    }
+
+    const toLeftMost = (arr) => {
+        const result = [...arr]
+        let indexToMoveFromRight = arr.length - 1
+
+        let i = 0
+        while (i < indexToMoveFromRight) {
+            if (result[i] === '.') {
+                result[i] = result[indexToMoveFromRight]
+                result[indexToMoveFromRight] = '.'
+
+                while (result[indexToMoveFromRight] === '.') {
+                    indexToMoveFromRight--
+                }
+            }
+            i++
+        }
+
+        return result
+    }
+
+    const getChecksum = (arr) => {
+        let i = 0
+        let checksum = 0
+        while (arr[i] !== '.') {
+            checksum += parseInt(arr[i]) * i
+
+            i++
+        }
+
+        return checksum
+    }
+
+    return getChecksum(toLeftMost(getP1(input)))
 }
 
 const part2 = (input) => {
